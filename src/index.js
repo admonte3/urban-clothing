d6d75c72b7f74f8fa43403d37800af85ffb8f4d7
Adrian Montes <admonte3@gmail.com>
2019-12-10 18:46:14 -0500

    Updated menu-items to navigate withRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { isMemberExpression } from '@babel/types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>,
+ document.getElementById('root'));
 
 
